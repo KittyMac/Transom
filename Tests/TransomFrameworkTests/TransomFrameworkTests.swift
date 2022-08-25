60561078907fa703443c4171c39f15fb6070a0cc
Rocco Bowling <rocco@chimerasw.com>
2022-08-25 00:50:39 -0400

    WIP: getting transom to reprocess reliably

--- Tests/TransomFrameworkTests/TransomFrameworkTests.swift
+++ Tests/TransomFrameworkTests/TransomFrameworkTests.swift
@@ -3,9 +3,16 @@ import TransomFramework
 
 final class TransomFrameworkTests: XCTestCase {
     
-    /*
+    func testBadCode() {
+        //let test = Blah()
+        if false {
+            
+        }
+    }
+    
     func testExample() throws {
-        TransomFramework.shared.translate(file: "/Users/rjbowli/Development/chimerasw/Transom/Tests/TransomFrameworkTests/Pamphlet/function0.swift",
-                                          outputDirectory: "/tmp/")
-    }*/
+        
+        TransomFramework.shared.translate(file: "/Users/rjbowli/Library/Developer/Xcode/DerivedData/Transom-hfbrfltfpvkvrscztuixgagzspfn/SourcePackages/transom/TransomFrameworkTests/TransomPlugin/inputFiles.txt",
+                                          outputDirectory: "/Users/rjbowli/Library/Developer/Xcode/DerivedData/Transom-hfbrfltfpvkvrscztuixgagzspfn/SourcePackages/transom/TransomFrameworkTests/TransomPlugin")
+    }
 }
